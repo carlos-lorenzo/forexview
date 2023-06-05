0b788cbfdf0a77425e1c8b8fcbbc62cc9ace9126
Carlator <clorenzozuniga@gmail.com>
2023-06-05 22:39:36 +0200

    - Improved js

--- Api/app.py
+++ Api/app.py
@@ -1,23 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 import pandas as pd
 
-import yfinance as yf
+from defractalise_ohlc import defractalise_ohlc
+
 
 app = Flask(__name__)
 CORS(app)
 
 app.debug = True
 
-df = yf.download('EURUSD=X', period="1d", interval="15m")
+
+df = pd.read_csv("Data/EURUSD_1m.csv", nrows=10000)
+
 df = df.rename(columns={"Open": "open",
                         "High": "high",
                         "Low": "low",
                         "Close": "close"})
 
-df["time"] = pd.to_datetime(df.index).astype('int64') / 10**9
-df = df.drop(["Adj Close", "Volume"], axis=1)
+df["time"] = pd.to_datetime(df["Time"]).astype('int64') / 10**9
+
+df = df.drop(["Volume", "Time"], axis=1)
 
 SCALAR = 1000
 
@@ -26,9 +30,19 @@ df["high"] *= SCALAR
 df["low"] *= SCALAR
 df["close"] *= SCALAR
 
-base_candle_number = 10 
-current_candle = base_candle_number
+time_frames = {
+    "1m": df,
+    "15m": defractalise_ohlc(df=df, interval="15m"),
+    "1h": defractalise_ohlc(df=df, interval="1h"),
+    "4h": defractalise_ohlc(df=df, interval="4h"),
+    "1D": defractalise_ohlc(df=df, interval="1D"),
+    "1W": defractalise_ohlc(df=df, interval="1W"),
+    "1M": defractalise_ohlc(df=df, interval="1M"),
+}
 
+
+base_candle_number = 100
+current_candle = base_candle_number
 
 @app.route("/")
 def index():
@@ -37,8 +51,15 @@ def index():
 
 @app.route("/api/get-chart-data", methods = ["POST", "GET"])
 def get_data():
+    global current_candle
     
-    chart_data = df.iloc[:base_candle_number].to_dict(orient='records')
+    current_candle = base_candle_number
+    
+    args = request.args
+    time_frame = args.get("tf", "1m")
+    
+    
+    chart_data = time_frames.get(time_frame).iloc[:base_candle_number].to_dict(orient='records')
     
     return jsonify(chart_data)
 
@@ -48,7 +69,11 @@ def update_current_candle():
     global current_candle
     current_candle += 1
     
-    return jsonify(df.iloc[current_candle].to_dict())
+    args = request.args
+    time_frame = args.get("tf", "1m")
+    
+    
+    return jsonify(time_frames.get(time_frame).iloc[current_candle].to_dict())
 
 if __name__ == '__main__':
     app.run()
--- Frontend/js/chart.js
+++ Frontend/js/chart.js
@@ -1,21 +1,11 @@
 import { createChart } from '/node_modules/lightweight-charts/dist/lightweight-charts.standalone.development.mjs';
 
-
+/*
 const button = document.getElementById('new-candle');
 
-const chartContainer = document.getElementById("chart-container")
-const chart = createChart(chartContainer, { width: 1000, height: 500 });
-const candlestickSeries = chart.addCandlestickSeries({
-    upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
-    wickUpColor: '#26a69a', wickDownColor: '#ef5350',
-});
-
-chart.priceScale('right').applyOptions({
-    ticksVisible: true
-})
 
 button.addEventListener('click', function () {
-    fetch("http://127.0.0.1:5000/api/new-candle", {
+    fetch("http://127.0.0.1:5000/api/new-candle?tf=15m", {
         method: "POST"})
 
     .then(response => response.json())
@@ -26,7 +16,7 @@ button.addEventListener('click', function () {
 });
 
 
-fetch("http://127.0.0.1:5000/api/get-chart-data", {
+fetch("http://127.0.0.1:5000/api/get-chart-data?tf=15m", {
         method: "POST"})
     .then(response => response.json())  
     .then(chartData => {
@@ -34,5 +24,69 @@ fetch("http://127.0.0.1:5000/api/get-chart-data", {
         chart.timeScale().fitContent(); 
 })
 
+*/
 
+
+
+function createCandle(timeFrame) {
+    const chartContainer = document.getElementById(timeFrame)
+    const chart = createChart(chartContainer, { width: 500, height: 500 });
+    const candlestickSeries = chart.addCandlestickSeries({
+        upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
+        wickUpColor: '#26a69a', wickDownColor: '#ef5350',
+    });
+
+    chart.priceScale('right').applyOptions({
+        ticksVisible: true
+    })
+
+    fetch(`http://127.0.0.1:5000/api/get-chart-data?tf=${timeFrame}`, {
+        method: "POST"})
+    .then(response => response.json())  
+    .then(chartData => {
+        candlestickSeries.setData(chartData);
+        chart.timeScale().fitContent(); 
+    })
+
+    return {
+        "chart": chart,
+        "candle": candlestickSeries
+    }
+}
+
+
+function updateChart(timeFrame){
+    return 1
+}
+
+
+const timeFrames = ["1m", "15m"]; // , "4h", "1h"
+var charts = {};
+
+var chartUpdates = [];
+
+timeFrames.forEach(timeFrame => {
+    charts[timeFrame] = createCandle(timeFrame);
+    chartUpdates.push({
+        "tf": timeFrame, 
+        "button": document.getElementById(`${timeFrame}-new-candle`)
+    });
+})
+
+chartUpdates.forEach(updateData => {
+
+    let timeFrame = updateData["tf"]
+    let button = updateData["button"]
+
+    button.addEventListener('click', function () {
+        fetch(`http://127.0.0.1:5000/api/new-candle?tf=${timeFrame}`, {
+            method: "POST"})
+    
+        .then(response => response.json())
+        .then(newCandleData => {
+            charts[timeFrame]["candle"].update(newCandleData);
+        
+        })
+    });
+})
 
