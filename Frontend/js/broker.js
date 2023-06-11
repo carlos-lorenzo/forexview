aab2e5f2042ea34db4d3670e87308ab3ec76e02e
Carlator <clorenzozuniga@gmail.com>
2023-06-11 20:53:34 +0200

    - Improved syling

--- Frontend/js/broker.js
+++ Frontend/js/broker.js
@@ -1,10 +1,10 @@
 function updateBroker() {
-    fetch(`http://127.0.0.1:5000/api/get-balance`, {
+    /*fetch(`http://127.0.0.1:5000/api/get-balance`, {
             method: "POST"})  
         .then(response => response.json())
         .then(balance => {
             document.getElementById("balance").innerHTML = `Balance: ${balance["balance"]}`;
-    })
+    })*/
 
     fetch(`http://127.0.0.1:5000/api/get-equity`, {
             method: "POST"})  
@@ -12,7 +12,8 @@ function updateBroker() {
         .then(equity => {
             document.getElementById("equity").innerHTML = `Equity: ${equity["equity"]}`;
     })
-
+    
+    
     showOpenPositions();
 
 }
@@ -21,20 +22,18 @@ updateBroker()
 
 export { updateBroker };
 
-function openPosition() {
+function openPosition(type) {
     let positionForm = document.forms["position-form"];
 
-    let type = positionForm["type"].value;
     let pair =  "EURUSD" //positionForm["pair"].value;
     let tp = positionForm["tp"].value;
     let sl = positionForm["sl"].value;
 
-    fetch(`http://127.0.0.1:5000/api/open-position?type=${type}&pair=${pair}&tp=${tp}&sl=${sl}`, {
-            method: "POST"})  
-        .then(response => response.json())
-        .then(meta => {
-            console.log(meta)
-    }) 
+    fetch(`http://127.0.0.1:5000/api/open-position?type=${type}&pair=${pair}&tp=${tp}&sl=${sl}`, {method: "POST"})
+    .then(response => response.json())
+    .then(status => {
+        updateBroker();
+    })
 }
 
 function showOpenPositions() {
@@ -55,12 +54,12 @@ function showOpenPositions() {
             positionBalance.className = "position-balance";
 
             let profitLoss = position["profit_loss"];
-            positionBalance.innerHTML = `${position["starting_size"] + profitLoss}`;
+            positionBalance.innerHTML = `${Math.round(position["starting_size"] + profitLoss)}`;
 
             if (profitLoss >= 0) {
-                positionBalance.style.cssText = "color: blue;";
+                positionBalance.style.cssText = "color: #2A32B9;";
             } else{
-                positionBalance.style.cssText = "color: red;";
+                positionBalance.style.cssText = "color: #EF5350;";
             }
             openPositionContainer.appendChild(positionBalance);
 
@@ -73,7 +72,7 @@ function showOpenPositions() {
 
             let closePositionButton = document.createElement("button");
             closePositionButton.className = "close-position";
-            closePositionButton.innerHTML = "Close Position";
+            closePositionButton.innerHTML = "Close";
             closePositionButton.addEventListener("click", function() {
                 fetch(`http://127.0.0.1:5000/api/close-position?id=${position["id"]}`, {method: "POST"})
                 .then(response => response.json())
@@ -91,7 +90,12 @@ function showOpenPositions() {
 }
 
 
-document.getElementById("open-position").addEventListener('click', function () {
-    openPosition();
-    updateBroker();
+document.getElementById("long").addEventListener('click', function () {
+    openPosition("long");
+    
+})
+
+document.getElementById("short").addEventListener('click', function () {
+    openPosition("short");
+    
 })
--- Frontend/js/chart.js
+++ Frontend/js/chart.js
@@ -2,32 +2,54 @@ import { createChart } from '/node_modules/lightweight-charts/dist/lightweight-c
 
 import { updateBroker } from "/Frontend/js/broker.js"
 
-document.getElementById("reset").addEventListener("click", function(){
-    fetch("http://127.0.0.1:5000/api/reset?pair=EURUSD", {
-        method: "POST"})
-    .then(function() {
-        updateBroker();
-        
-
-    })
-})
 
 
 function createCandleChart(timeFrame) {
-    const chartContainer = document.getElementById(timeFrame)
-    chartContainer.innerHTML = "";
-    const chart = createChart(chartContainer, { width: 500, height: 500 });
-    const candlestickSeries = chart.addCandlestickSeries({
-        upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
-        wickUpColor: '#26a69a', wickDownColor: '#ef5350', priceFormat: { type: 'price', precision: 5, minMove: 0.00001 }
+    let chartContainer = document.createElement("div");
+    chartContainer.className = "chart";
+    chartContainer.id = timeFrame;
+    console.log();
+    let chartWidth = (window.innerWidth * 0.4)
+    let chartHeight = (window.innerHeight * 0.6)
+
+    const chart = createChart(chartContainer, {
+        height: chartHeight, 
+        width: chartWidth,
+        layout: {
+            background: {
+                color: '#131722',  
+            },
+            textColor: "white",
+            fontFamily: "'Orbitron', sans-serif"
+        }
+        
+    
     });
+
+    
+
+    const candlestickSeries = chart.addCandlestickSeries({
+        upColor: '#26a69a', 
+        downColor: '#ef5350', 
+        borderVisible: true,
+        wickUpColor: '#26a69a', 
+        wickDownColor: '#ef5350', 
+        priceFormat: { type: 'price', precision: 5, minMove: 0.00001 },
+        
+    });
+
 
     chart.priceScale('right').applyOptions({
         ticksVisible: true,
     })
 
+    chart.applyOptions({
+        background: {type: "solid", "color": "red"}
+    })
+
     candlestickSeries.applyOptions({
-        
+        title: timeFrame,
+       
     })
 
     fetch(`http://127.0.0.1:5000/api/get-chart-data?tf=${timeFrame}&pair=EURUSD`, {
@@ -38,6 +60,11 @@ function createCandleChart(timeFrame) {
         chart.timeScale().fitContent();
 
     })
+
+    let chartsContainer = document.getElementById("charts");
+
+    chartsContainer.appendChild(chartContainer);
+
 
     return {
         "chart": chart,
@@ -64,54 +91,106 @@ function updateChart(timeFrame,  candlestickSeries){
     })
 }
 
+function createCharts(timeFrames) {
+    charts = {};
+    timeFrames.forEach(timeFrame => {
+        charts[timeFrame] = createCandleChart(timeFrame);
+    })
 
-const timeFrames = ["1m", "15m", "4h", "1D"];
-var charts = {};
+    return charts;
+}
 
-var chartUpdates = [];
 
-timeFrames.forEach(timeFrame => {
-    charts[timeFrame] = createCandleChart(timeFrame);
-    chartUpdates.push({
-        "tf": timeFrame, 
-        "button": document.getElementById(`${timeFrame}-new-candle`)
-    });
-})
+function setTimeDials(timeFrames, charts) {
+    let timeDials = document.getElementById("time-dials");
+    timeDials.innerHTML = "";
+    
+    timeFrames.forEach(timeFrame => {
+        let newCandleButton = document.createElement("button");
+        newCandleButton.innerHTML = `Add ${timeFrame}`
+        newCandleButton.addEventListener("click", function () {
 
-chartUpdates.forEach(updateData => {
+            fetch(`http://127.0.0.1:5000/api/update-time?tf=${timeFrame}&pair=EURUSD`, {method: "GET"})
 
-    let timeFrame = updateData["tf"]
-    let button = updateData["button"]
+            for (let chartTimeFrame in charts) {
+                let candlestickSeries = charts[chartTimeFrame]["candle"];
 
-    button.addEventListener('click', function () {
-
-        fetch(`http://127.0.0.1:5000/api/update-time?tf=${timeFrame}&pair=EURUSD`, {method: "GET"})
-        
-        for (let chartTimeFrame in charts) {
-
-            let candlestickSeries = charts[chartTimeFrame]["candle"];
-
-            if (chartTimeFrame == timeFrame) {
-                updateChart(chartTimeFrame, candlestickSeries);  
-            } else {
-                setChartData(chartTimeFrame, candlestickSeries);
+                if (chartTimeFrame == timeFrame) {
+                    updateChart(chartTimeFrame, candlestickSeries);  
+                } else {
+                    setChartData(chartTimeFrame, candlestickSeries);
+                }
             }
-        }
+        })
+
+        timeDials.appendChild(newCandleButton);
+
+    })
+}
+
+function drawTimeFrameSelectors(validTimeFrames) {
+    const timeFrameSelectors = document.getElementById("timeframe-selectors");
+    timeFrameSelectors.innerHTML = "";
+    validTimeFrames.forEach(timeFrame => {
+        let timeFrameSelector = document.createElement("div");
+        timeFrameSelector.className = "timeframe-selector";
+        timeFrameSelector.id = `${timeFrame}-selector`
+        timeFrameSelector.onclick = openTimeFrame;
         
-        updateBroker()
-    });
-})
+        timeFrameSelector.innerHTML = timeFrame;
+
+        let currentTimeFrame = fetchTimeFrameToDraw(validTimeFrames)[0];
+        if (timeFrame == currentTimeFrame) {
+            timeFrameSelector.style.color = "#1E53E5";
+        } else {
+            timeFrameSelector.style.color = "white";
+        }
+        timeFrameSelector.style.cursor = "pointer";
+        
+
+        timeFrameSelectors.appendChild(timeFrameSelector);
+    })
+}
+
+function openTimeFrame(pointerEvent) {
+    
+    window.open(`http://127.0.0.1:5500/Frontend/html/?tf=${pointerEvent.target.innerHTML}`, "_self");
+    
+}
+
+function fetchTimeFrameToDraw(validTimeFrames) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const timeFrame = urlParams.get("tf")
+
+    if (validTimeFrames.includes(timeFrame)){
+        
+        return [timeFrame];
+    } else {
+        return ["15m"]
+    }
+    
+}
+
+
+
+const validTimeFrames = ["1m", "15m", "4h", "1D"]
+const timeFrames = fetchTimeFrameToDraw(validTimeFrames);
+
+
+var charts = createCharts(timeFrames);
+drawTimeFrameSelectors(validTimeFrames);
+setTimeDials(validTimeFrames, charts);
 
 document.getElementById("reset").addEventListener("click", function(){
     fetch("http://127.0.0.1:5000/api/reset?pair=EURUSD", {
         method: "POST"})
     .then(function() {
+        document.getElementById("charts").innerHTML = "";
+        document.getElementById("time-dials").innerHTML = "";
         updateBroker();
         
-        charts = {};
-
-        timeFrames.forEach(timeFrame => {
-            charts[timeFrame] = createCandleChart(timeFrame);
-        })
+        charts = createCharts(timeFrames);
+        drawTimeFrameSelectors(validTimeFrames);
+        setTimeDials(validTimeFrames, charts);
     })
 })
